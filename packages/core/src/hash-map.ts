8ac0c7d4f14f0de3f8ecccf840fe98e5c51a6570
Yannick Stachelscheid <yannick@pricenow.ch>
2024-02-04 13:57:39 +0100

    feature: first draft of route definition and bag of routes incl. builder

---
+++ packages/core/src/hash-map.ts
@@ -0,0 +1,102 @@
+type HashFunction<TKey> = (input: TKey) => string;
+
+export class HashMap<TKey, TValue> {
+  // internal map representing the hash map
+  private readonly _map = new Map<string, TValue>();
+  // remember original key values
+  private readonly _keys = new Map<string, TKey>();
+
+  private readonly hashFunc: HashFunction<TKey>;
+
+  constructor(
+    hashFunc: HashFunction<TKey>,
+    entries: readonly (readonly [TKey, TValue])[]
+  );
+  constructor(hashFunc: HashFunction<TKey>);
+  constructor(
+    hashFunc: HashFunction<TKey>,
+    entries?: readonly (readonly [TKey, TValue])[]
+  ) {
+    this.hashFunc = hashFunc;
+
+    this._map.entries;
+
+    Array.from(entries ?? []).forEach(([key, value]) => this.set(key, value));
+  }
+
+  set(key: TKey, value: TValue) {
+    this._map.set(this.hashFunc(key), value);
+    this._keys.set(this.hashFunc(key), key);
+
+    return this;
+  }
+
+  get(key: TKey): TValue | undefined {
+    return this._map.get(this.hashFunc(key));
+  }
+
+  has(key: TKey): boolean {
+    return this._map.has(this.hashFunc(key));
+  }
+
+  delete(key: TKey): boolean {
+    if (this.has(key)) {
+      this._map.delete(this.hashFunc(key));
+      this._keys.delete(this.hashFunc(key));
+
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  clear(): void {
+    this._map.clear();
+    this._keys.clear();
+  }
+
+  keys(): IterableIterator<TKey> {
+    return this._keys.values();
+  }
+
+  values(): IterableIterator<TValue> {
+    return this._map.values();
+  }
+
+  get size(): number {
+    return this._map.size;
+  }
+
+  entries(): IterableIterator<[TKey, TValue]> {
+    const entries = Array.from(this._map.entries());
+    let nextIndex = 0;
+
+    const it = {
+      [Symbol.iterator]: () => it, // @todo find out how to properly solve this
+      next: () => {
+        if (nextIndex < entries.length) {
+          const [stringKey, value] = entries[nextIndex++];
+          const key = this._keys.get(stringKey);
+
+          if (!key) {
+            throw new Error(
+              `Could not map back string key ${stringKey} to original key`
+            );
+          }
+
+          return {
+            done: false as const,
+            value: [key, value] as [TKey, TValue],
+          };
+        } else {
+          return {
+            done: true as const,
+            value: undefined,
+          };
+        }
+      },
+    };
+
+    return it;
+  }
+}
---
+++ packages/core/src/index.ts
@@ -0,0 +1,197 @@
+import { z } from "zod";
+import { HashMap } from "./hash-map";
+
+export type HTTPMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE"; // @todo add remaining types
+
+const responseType = Symbol("Response Type");
+export type ResponseTypeKey = typeof responseType;
+
+export class RouteDef<
+  TVersion extends string,
+  TMethod extends HTTPMethod,
+  TPath extends string,
+  TValidator extends z.ZodTypeAny,
+  TResponse
+> {
+  public readonly version: TVersion;
+  public readonly method: TMethod;
+  public readonly path: TPath;
+  public readonly validator: TValidator;
+  public readonly [responseType]: TResponse;
+
+  constructor(
+    version: TVersion,
+    method: TMethod,
+    path: TPath,
+    validator: TValidator
+  ) {
+    this.version = version;
+    this.method = method;
+    this.path = path;
+    this.validator = validator;
+    this[responseType] = null as any;
+  }
+}
+
+export class Route {
+  public version<TVersion extends string>(version: TVersion) {
+    return new RouteWithVersion(version);
+  }
+
+  public get<TPath extends string>(path: TPath) {
+    return new RouteBuilderWithVersionAndMethodAndPath("", "GET", path);
+  }
+}
+
+export class RouteWithVersion<TVersion extends string> {
+  constructor(private version: TVersion) {}
+
+  public get<TPath extends string>(path: TPath) {
+    return new RouteBuilderWithVersionAndMethodAndPath(
+      this.version,
+      "GET",
+      path
+    );
+  }
+}
+
+export class RouteBuilderWithVersionAndMethodAndPath<
+  TVersion extends string,
+  TMethod extends HTTPMethod,
+  TPath extends string
+> {
+  constructor(
+    private version: TVersion,
+    private method: TMethod,
+    private path: TPath
+  ) {}
+
+  public validate<TValidator extends z.ZodTypeAny>(validator: TValidator) {
+    return new RouteBuilderWithVersionAndMethodAndPathAndValidator<
+      TVersion,
+      TMethod,
+      TPath,
+      TValidator
+    >(this.version, this.method, this.path, validator);
+  }
+
+  public response<TResponse>() {
+    return new RouteDef<TVersion, TMethod, TPath, z.ZodTypeAny, TResponse>(
+      this.version,
+      this.method,
+      this.path,
+      z.any()
+    );
+  }
+}
+
+export class RouteBuilderWithVersionAndMethodAndPathAndValidator<
+  TVersion extends string,
+  TMethod extends HTTPMethod,
+  TPath extends string,
+  TValidator extends z.ZodTypeAny
+> {
+  constructor(
+    private version: TVersion,
+    private method: TMethod,
+    private path: TPath,
+    private validator: TValidator
+  ) {}
+
+  public response<TResponse>() {
+    return new RouteDef<TVersion, TMethod, TPath, TValidator, TResponse>(
+      this.version,
+      this.method,
+      this.path,
+      this.validator
+    );
+  }
+}
+
+export type AnyRouteDef = RouteDef<
+  string,
+  HTTPMethod,
+  string,
+  z.ZodTypeAny,
+  any
+>;
+
+export enum Versioning {
+  NO_VERSIONING,
+  DATE,
+  SEMVER,
+}
+
+export class BagOfRoutesBuilder {
+  public withVersioning<TVersioning extends Versioning>(
+    versioning: TVersioning
+  ) {
+    return new BagOfRoutesBuilderWithVersioning<never, TVersioning>(versioning);
+  }
+
+  public withoutVersioning() {
+    return new BagOfRoutesBuilderWithVersioning<
+      never,
+      Versioning.NO_VERSIONING
+    >(Versioning.NO_VERSIONING);
+  }
+}
+
+export class BagOfRoutesBuilderWithVersioning<
+  TRoutes extends AnyRouteDef,
+  TVersioning extends Versioning
+> {
+  protected routes: HashMap<[HTTPMethod, string], AnyRouteDef> = new HashMap<
+    [HTTPMethod, string],
+    AnyRouteDef
+  >((key) => key.join("-"));
+  private versioning: TVersioning;
+
+  constructor(versioning: TVersioning) {
+    this.versioning = versioning;
+  }
+
+  public addRoute<TRouteDef extends AnyRouteDef>(
+    route: TRouteDef
+  ): BagOfRoutesBuilderWithVersioning<TRoutes | TRouteDef, TVersioning> {
+    this.routes.set([route.method, route.path], route);
+    return this;
+  }
+
+  public build() {
+    return new BagOfRoutes<TRoutes, TVersioning>(this.routes, this.versioning);
+  }
+}
+
+export class BagOfRoutes<
+  TRoutes extends AnyRouteDef,
+  TVersioning extends Versioning
+> {
+  public readonly routes: HashMap<[HTTPMethod, string], AnyRouteDef>;
+  public readonly versioning: TVersioning;
+
+  constructor(
+    routes: HashMap<[HTTPMethod, string], AnyRouteDef>,
+    versioning: TVersioning
+  ) {
+    this.routes = routes;
+    this.versioning = versioning;
+  }
+}
+
+const bagOfRoutes = new BagOfRoutesBuilder()
+  .withVersioning(Versioning.DATE)
+  .addRoute(
+    new Route()
+      .version("2024-01-01")
+      .get("/basket")
+      .response<{ id: string; entries: any[] }>()
+  )
+  .addRoute(
+    new Route()
+      .version("2024-01-01")
+      .get("/basket/:basketId/entries")
+      .validate(z.object({ params: z.object({ basketId: z.string() }) }))
+      .response<any[]>()
+  )
+  .build();
