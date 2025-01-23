3a2bbd43618578aa3205c5dd5d80dd8f99851004
Yannick Stachelscheid <yannick@pricenow.ch>
2025-01-23 14:26:36 +0100

    fix: try to use triple slash directives in express-server index.ts

--- packages/express-server/src/index.ts
+++ packages/express-server/src/index.ts
@@ -1,3 +1,6 @@
+/// <reference types="express-serve-static-core" />
+/// <reference types="@t-rest/core" />
+
 // classes
 export * from './classes/typed-express-application'
 
@@ -11,11 +14,11 @@ export * from './utils/define-middleware'
 
 // core
 // https://github.com/microsoft/TypeScript/issues/42873#issuecomment-2040902002
-export * from '@t-rest/core'
-export {
-  Router as _ExpressRouter,
-  RequestHandler as _ExpressRequestHandler,
-  Request as _ExpressRequest,
-  Response as _ExpressResponse,
-  Application as _ExpressApplication,
-} from 'express-serve-static-core'
+//export * from '@t-rest/core'
+//export {
+//  Router as _ExpressRouter,
+//  RequestHandler as _ExpressRequestHandler,
+//  Request as _ExpressRequest,
+//  Response as _ExpressResponse,
+//  Application as _ExpressApplication,
+//} from 'express-serve-static-core'
