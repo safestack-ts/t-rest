c582103b192fd39c4b2969e2442f988cc997bb67
Yannick Stachelscheid <yannick@pricenow.ch>
2025-01-23 14:16:27 +0100

    fix: expose express-serve-static-core re-exports as non-conflicting alias

--- packages/express-server/src/index.ts
+++ packages/express-server/src/index.ts
@@ -13,9 +13,9 @@ export * from './utils/define-middleware'
 // https://github.com/microsoft/TypeScript/issues/42873#issuecomment-2040902002
 export * from '@t-rest/core'
 export {
-  Router,
-  RequestHandler,
-  Request,
-  Response,
-  Application,
+  Router as _ExpressRouter,
+  RequestHandler as _ExpressRequestHandler,
+  Request as _ExpressRequest,
+  Response as _ExpressResponse,
+  Application as _ExpressApplication,
 } from 'express-serve-static-core'
