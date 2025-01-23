cd1b7facce3cf9f1c2a284d065f0acbf8f90ef33
Yannick Stachelscheid <yannick@pricenow.ch>
2025-01-23 15:31:13 +0100

    fix: remove re-exports from express-server again

--- packages/express-server/src/index.ts
+++ packages/express-server/src/index.ts
@@ -1,6 +1,3 @@
-/// <reference types="express-serve-static-core" />
-/// <reference types="@t-rest/core" />
-
 // classes
 export * from './classes/typed-express-application'
 
@@ -14,11 +11,11 @@ export * from './utils/define-middleware'
 
 // core
 // https://github.com/microsoft/TypeScript/issues/42873#issuecomment-2040902002
-//export * from '@t-rest/core'
-//export {
-//  Router as _ExpressRouter,
-//  RequestHandler as _ExpressRequestHandler,
-//  Request as _ExpressRequest,
-//  Response as _ExpressResponse,
-//  Application as _ExpressApplication,
-//} from 'express-serve-static-core'
+/*export * from '@t-rest/core'
+export {
+  Router as _ExpressRouter,
+  RequestHandler as _ExpressRequestHandler,
+  Request as _ExpressRequest,
+  Response as _ExpressResponse,
+  Application as _ExpressApplication,
+} from 'express-serve-static-core'*/
