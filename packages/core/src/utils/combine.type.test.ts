ccd38999107cb19289f94cf6dc41f8d3072fe84b
Yannick Stachelscheid <yannick@pricenow.ch>
2025-02-13 07:54:25 +0100

    feature: combine() test case with versioned bag of route

--- packages/core/src/utils/combine.type.test.ts
+++ packages/core/src/utils/combine.type.test.ts
@@ -4,6 +4,8 @@ import { AssertFalse, IsNever } from 'conditional-type-checks'
 import { BagOfRoutes } from '../classes/core/bag-of-routes'
 import { Route } from '../classes/core/route'
 import { ExtractRoutes } from '../types/extract-route'
+import { Versioning } from '../enums/versioning'
+import { versionHistory } from './dx/demo-bag-of-routes'
 
 type User = {
   id: string
@@ -16,34 +18,82 @@ type Basket = {
   priceTotal: number
 }
 
-const bagOfRoutesUsers = BagOfRoutes.withoutVersioning()
-  .addRoute(Route.get('/users').response<User[]>())
-  .addRoute(Route.post('/users').response<User>())
-  .build()
+namespace _WithoutVersioning {
+  const bagOfRoutesUsers = BagOfRoutes.withoutVersioning()
+    .addRoute(Route.get('/users').response<User[]>())
+    .addRoute(Route.post('/users').response<User>())
+    .build()
 
-const bagOfRoutesBaskets = BagOfRoutes.withoutVersioning()
-  .addRoute(
-    Route.get('/baskets/:basketId')
-      .validate(z.object({ params: z.object({ basketId: z.string() }) }))
-      .response<Basket>()
+  const bagOfRoutesBaskets = BagOfRoutes.withoutVersioning()
+    .addRoute(
+      Route.get('/baskets/:basketId')
+        .validate(z.object({ params: z.object({ basketId: z.string() }) }))
+        .response<Basket>()
+    )
+    .addRoute(Route.post('/baskets').response<Basket>())
+    .build()
+
+  const bagOfRoutes = combine(bagOfRoutesUsers, bagOfRoutesBaskets)
+  type Routes = ExtractRoutes<typeof bagOfRoutes>
+
+  // every route should be part of the combined bag
+  type GetUsersRoute = Extract<Routes, { method: 'GET'; path: '/users' }>
+  type PostUsersRoute = Extract<Routes, { method: 'POST'; path: '/users' }>
+  type GetBasketsRoute = Extract<
+    Routes,
+    { method: 'GET'; path: '/baskets/:basketId' }
+  >
+  type PostBasketsRoute = Extract<Routes, { method: 'POST'; path: '/baskets' }>
+
+  type _test =
+    | AssertFalse<IsNever<GetUsersRoute>>
+    | AssertFalse<IsNever<PostUsersRoute>>
+    | AssertFalse<IsNever<GetBasketsRoute>>
+    | AssertFalse<IsNever<PostBasketsRoute>>
+}
+
+namespace _WithVersioning {
+  const bagOfRoutesUsers = BagOfRoutes.withVersioning(
+    Versioning.DATE,
+    versionHistory
   )
-  .addRoute(Route.post('/baskets').response<Basket>())
-  .build()
+    .addRoute(Route.version('2024-01-01').get('/users').response<User[]>())
+    .addRoute(Route.version('2024-01-01').post('/users').response<User>())
+    .build()
 
-const bagOfRoutes = combine(bagOfRoutesUsers, bagOfRoutesBaskets)
-type Routes = ExtractRoutes<typeof bagOfRoutes>
+  const bagOfRoutesBaskets = BagOfRoutes.withVersioning(
+    Versioning.DATE,
+    versionHistory
+  )
+    .addRoute(
+      Route.version('2024-01-01').get('/baskets/:basketId').response<Basket>()
+    )
+    .addRoute(Route.version('2024-01-01').post('/baskets').response<Basket>())
+    .build()
 
-// every route should be part of the combined bag
-type GetUsersRoute = Extract<Routes, { method: 'GET'; path: '/users' }>
-type PostUsersRoute = Extract<Routes, { method: 'POST'; path: '/users' }>
-type GetBasketsRoute = Extract<
-  Routes,
-  { method: 'GET'; path: '/baskets/:basketId' }
->
-type PostBasketsRoute = Extract<Routes, { method: 'POST'; path: '/baskets' }>
+  const bagOfRoutes = combine(bagOfRoutesUsers, bagOfRoutesBaskets)
+  type Routes = ExtractRoutes<typeof bagOfRoutes>
 
-type _test =
-  | AssertFalse<IsNever<GetUsersRoute>>
-  | AssertFalse<IsNever<PostUsersRoute>>
-  | AssertFalse<IsNever<GetBasketsRoute>>
-  | AssertFalse<IsNever<PostBasketsRoute>>
+  type GetUsersRoute = Extract<
+    Routes,
+    { method: 'GET'; path: '/users'; version: '2024-01-01' }
+  >
+  type PostUsersRoute = Extract<
+    Routes,
+    { method: 'POST'; path: '/users'; version: '2024-01-01' }
+  >
+  type GetBasketsRoute = Extract<
+    Routes,
+    { method: 'GET'; path: '/baskets/:basketId'; version: '2024-01-01' }
+  >
+  type PostBasketsRoute = Extract<
+    Routes,
+    { method: 'POST'; path: '/baskets'; version: '2024-01-01' }
+  >
+
+  type _test =
+    | AssertFalse<IsNever<GetUsersRoute>>
+    | AssertFalse<IsNever<PostUsersRoute>>
+    | AssertFalse<IsNever<GetBasketsRoute>>
+    | AssertFalse<IsNever<PostBasketsRoute>>
+}
