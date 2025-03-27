dc844a90baa6ad4410571bd4b7202f7949188aff
Yannick Stachelscheid <yannick@pricenow.ch>
2025-03-27 12:55:38 +0100

    fix: express server route handle middleware handling abort chain on error

--- packages/express-server/src/__test__/express-routing-with-date-versioning.integration.test.ts
+++ packages/express-server/src/__test__/express-routing-with-date-versioning.integration.test.ts
@@ -9,9 +9,16 @@ import request from 'supertest'
 import { StatusCodes } from 'http-status-codes'
 import { DateVersionExtractor } from '../types/date-version-extractor'
 import { TypedExpressApplication } from '../classes/typed-express-application'
-import { ExpressApp, ExpressRequest } from '../types/express-type-shortcuts'
+import {
+  ExpressApp,
+  ExpressNextFunction,
+  ExpressRequest,
+  ExpressResponse,
+} from '../types/express-type-shortcuts'
 import { BagOfRoutes, Route, Versioning, ze } from '@t-rest/core'
 import { z } from 'zod'
+import { defineMiddleware } from '../utils/define-middleware'
+import 'express-async-errors'
 
 const { bagOfRoutes: baseBagOfRoutes, versionHistory } =
   TestBagOfRoutesWithVersioning
@@ -313,4 +320,67 @@ test('query params and path params are kept separate', async () => {
     data: { id: 1337, email: `user-1337@email.com` },
     version: '2024-01-01',
   })
+})
+
+test.only('middleware in router should abort middleware chain if error is returned', async () => {
+  type RequestWithClientId = {
+    auth: {
+      clientId: number
+    }
+  }
+  const withClientId = defineMiddleware<ExpressRequest, RequestWithClientId>(
+    (req, _res, next) => {
+      next?.(new Error('ClientId is missing'))
+      return
+    }
+  )
+
+  const bagOfRoutes = BagOfRoutes.withVersioning(
+    Versioning.DATE,
+    versionHistory
+  )
+    .addRoute(
+      Route.version('2024-01-01')
+        .get('/users/:userId')
+        .response<ResponseWithVersion<User>>()
+    )
+    .build()
+
+  const expressApp = Express()
+
+  const typedExpressApplication = TypedExpressApplication.withVersioning(
+    expressApp,
+    bagOfRoutes,
+    versionHistory,
+    new APIVersionHeaderExtractor()
+  )
+
+  typedExpressApplication
+    .get('/users/:userId')
+    .version('2024-01-01')
+    .middleware(withClientId)
+    .handle(async (__, _, response) => {
+      response.status(200).json({
+        version: '2024-01-01',
+        data: { id: 1337, email: `user-1337@email.com` },
+      })
+    })
+
+  expressApp.use(
+    (
+      err: Error,
+      req: ExpressRequest,
+      res: ExpressResponse,
+      _next: ExpressNextFunction
+    ) => {
+      res.status(500).send({ error: err.message })
+    }
+  )
+
+  const response = await request(expressApp)
+    .get('/users/1337')
+    .set('X-API-Version', '2024-01-01')
+    .expect(StatusCodes.INTERNAL_SERVER_ERROR)
+
+  expect(response.body).toEqual({ error: 'ClientId is missing' })
 })
