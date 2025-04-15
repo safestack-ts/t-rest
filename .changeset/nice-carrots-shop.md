924bc298606ebb96089c58d458c7080935a43c24
Yannick Stachelscheid <yannick@pricenow.ch>
2025-04-15 14:07:14 +0200

    fix: open api generator export OpenAPIMetaData type

---
+++ .changeset/nice-carrots-shop.md
@@ -0,0 +1,5 @@
+---
+'@t-rest/open-api-generator': patch
+---
+
+export OpenAPIMetaData type
--- packages/open-api-generator/src/index.ts
+++ packages/open-api-generator/src/index.ts
@@ -6,3 +6,6 @@ export * from './classes/open-api-spec'
 export * from './schema/type-schema'
 export * from './schema/route-meta'
 export * from './schema/header'
+
+// types
+export { OpenAPIMetaData } from './types/open-api-meta-data'
